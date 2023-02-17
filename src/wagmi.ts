a8391615d06d5e6377da89d99f35a6cf3ac79dbd
pythonpete32 <aaron@blackscale.io>
2023-02-18 00:41:54 +0400

    edit wagmi

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,25 +1,40 @@
-import { getDefaultWallets } from '@rainbow-me/rainbowkit'
-import { configureChains, createClient } from 'wagmi'
-import { goerli, mainnet } from 'wagmi/chains'
-import { publicProvider } from 'wagmi/providers/public'
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
+import {
+  injectedWallet,
+  rainbowWallet,
+  walletConnectWallet,
+  metaMaskWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { configureChains, createClient } from "wagmi";
+import { goerli, mainnet } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+import { alchemyProvider } from "wagmi/providers/alchemy";
 
 const { chains, provider, webSocketProvider } = configureChains(
-  [mainnet, ...(import.meta.env?.MODE === 'development' ? [goerli] : [])],
+  [mainnet, goerli],
   [
     publicProvider(),
-  ],
-)
+    alchemyProvider({ apiKey: process.env.VITE_ALCHEMY_API_KEY as string }),
+  ]
+);
 
-const { connectors } = getDefaultWallets({
-  appName: 'My wagmi + RainbowKit App',
-  chains,
-})
+const connectors = connectorsForWallets([
+  {
+    groupName: "Recommended",
+    wallets: [
+      injectedWallet({ chains }),
+      rainbowWallet({ chains }),
+      walletConnectWallet({ chains }),
+      metaMaskWallet({ chains }),
+    ],
+  },
+]);
 
 export const client = createClient({
   autoConnect: true,
   connectors,
   provider,
   webSocketProvider,
-})
+});
 
-export { chains }
+export { chains };
