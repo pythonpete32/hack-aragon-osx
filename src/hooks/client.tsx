a0c980d4fd8f6648ea9bda36ca23ca1765869c17
pythonpete32 <aaron@blackscale.io>
2023-02-21 00:50:11 +0400

    add hooks

---
+++ src/hooks/client.tsx
@@ -0,0 +1,92 @@
+import {
+  Client,
+  DaoSortBy,
+  IDaoQueryParams,
+  ITransferQueryParams,
+  SortDirection,
+  TransferSortBy,
+  TransferType,
+} from "@aragon/sdk-client";
+import { DaoBalancesQueryParams } from "@aragon/sdk-client/dist/interfaces";
+import { useQuery } from "react-query";
+import { useAragonSDKContext } from "../lib/AragonContext";
+
+function useClient() {
+  const { context } = useAragonSDKContext();
+  return new Client(context);
+}
+
+export function useFetchDaos(queryParams: IDaoQueryParams = {}) {
+  const client = useClient();
+
+  return useQuery({
+    queryKey: ["daos", queryParams],
+    queryFn: async () =>
+      client.methods.getDaos({
+        skip: 0,
+        limit: 10,
+        direction: SortDirection.ASC,
+        sortBy: DaoSortBy.POPULARITY,
+        ...queryParams,
+      }),
+    enabled: !!client,
+    onError(err) {
+      console.log({ "❌ useFetchDaos()": err });
+    },
+  });
+}
+
+export function useFetchDao(daoAddressOrEns: string) {
+  const client = useClient();
+
+  return useQuery({
+    queryKey: ["dao", daoAddressOrEns],
+    queryFn: async () => client.methods.getDao(daoAddressOrEns),
+    enabled: !!client,
+    onError(err) {
+      console.log({ "❌ useFetchDao()": err });
+    },
+  });
+}
+
+export function useFetchDaoBalances(queryParams: DaoBalancesQueryParams = {}) {
+  const client = useClient();
+
+  return useQuery({
+    queryKey: ["daoBalance", queryParams],
+    queryFn: async () =>
+      await client.methods.getDaoBalances({
+        daoAddressOrEns: "",
+        skip: 0,
+        limit: 10,
+        direction: SortDirection.ASC,
+        ...queryParams,
+      }),
+    enabled: !!client && !!queryParams.daoAddressOrEns,
+    onError(err) {
+      console.log({ "❌ useFetchDaoBalances()": err });
+    },
+  });
+}
+
+export function useFetchTransfers(queryParams: ITransferQueryParams = {}) {
+  const client = useClient();
+
+  return useQuery({
+    queryKey: ["daoTransfers", queryParams],
+    queryFn: async () =>
+      client.methods.getDaoTransfers({
+        daoAddressOrEns: "",
+        sortBy: TransferSortBy.CREATED_AT, // optional
+        limit: 10, // optional
+        skip: 0, // optional
+        direction: SortDirection.ASC, // optional
+        type: TransferType.DEPOSIT, // optional
+        ...queryParams,
+      }),
+    enabled: !!client && !!queryParams.daoAddressOrEns,
+    onError(err) {
+      console.log({ "❌ useFetchDaos()": err });
+    },
+  });
+}
---
+++ src/hooks/tokenVoting.tsx
@@ -0,0 +1,81 @@
+import {
+  Context,
+  ContextPlugin,
+  IProposalQueryParams,
+  ProposalSortBy,
+  ProposalStatus,
+  SortDirection,
+  TokenVotingClient,
+} from "@aragon/sdk-client";
+import { useQuery } from "react-query";
+import { useAragonSDKContext } from "../lib/AragonContext";
+
+function useTokenVotingClient() {
+  const { context } = useAragonSDKContext();
+
+  const contextPlugin: ContextPlugin = ContextPlugin.fromContext(
+    new Context(context)
+  );
+
+  return new TokenVotingClient(contextPlugin);
+}
+
+export function useFetchMembers(daoAddressOrEns: string) {
+  const client = useTokenVotingClient();
+
+  return useQuery({
+    queryKey: ["members", daoAddressOrEns],
+    queryFn: async () => client.methods.getMembers(daoAddressOrEns),
+    enabled: !!client,
+    onError(err) {
+      console.log({ "❌ useFetchMembers()": err });
+    },
+  });
+}
+
+export function useFetchProposals(queryParams: IProposalQueryParams = {}) {
+  const client = useTokenVotingClient();
+
+  return useQuery({
+    queryKey: ["proposals", queryParams],
+    queryFn: async () =>
+      await client.methods.getProposals({
+        skip: 0, // optional
+        limit: 10, // optional,
+        direction: SortDirection.ASC, // optional
+        sortBy: ProposalSortBy.POPULARITY, //optional
+        status: ProposalStatus.ACTIVE, // optional
+        ...queryParams,
+      }),
+    enabled: !!client && !!queryParams.daoAddressOrEns,
+    onError(err) {
+      console.log({ "❌ useFetchProposals()": err });
+    },
+  });
+}
+
+export function useFetchProposal(proposalId: string) {
+  const client = useTokenVotingClient();
+
+  return useQuery({
+    queryKey: ["proposal", proposalId],
+    queryFn: async () => await client.methods.getProposal(proposalId),
+    enabled: !!client,
+    onError(err) {
+      console.log({ "❌ useFetchProposal()": err });
+    },
+  });
+}
+
+export function useFetchVoteSettings(pluginAddress: string) {
+  const client = useTokenVotingClient();
+
+  return useQuery({
+    queryKey: ["voteSettings", pluginAddress],
+    queryFn: async () => await client.methods.getVotingSettings(pluginAddress),
+    enabled: !!client,
+    onError(err) {
+      console.log({ "❌ useFetchVoteSettings()": err });
+    },
+  });
+}
