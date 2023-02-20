40195cb4172ca60c582fe326d47b525756621da8
pythonpete32 <aaron@blackscale.io>
2023-02-21 00:49:33 +0400

    add react query

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,24 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { WagmiConfig } from "wagmi";
 
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
 import { App } from "./App";
 import { chains, client } from "./wagmi";
 import { AragonSDKWrapper } from "./lib/AragonContext";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <WagmiConfig client={client}>
       <RainbowKitProvider chains={chains} theme={darkTheme()}>
         <AragonSDKWrapper>
-          <App />
+          <QueryClientProvider client={queryClient}>
+            <App />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
         </AragonSDKWrapper>
       </RainbowKitProvider>
     </WagmiConfig>
